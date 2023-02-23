be0354a85c460debb0d4df376c645f18ed8d33a6
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-02-23 20:05:53 +0300

    Completed assignment

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,24 @@
 class Character < ActiveRecord::Base
+    belongs_to :actor
+    belongs_to :show
+
+    # def actor 
+    #     self.actors.map do |actor|
+    #         self.actor
+    #     end
+    # end
+
+    # def show 
+    #     self.shows.map do |show|
+    #         self.show
+    #     end
+    # end
+
+    
+
+    def say_that_thing_you_say 
+       "#{self.name} always says: #{self.catchphrase}" 
+    end
+
   
 end
